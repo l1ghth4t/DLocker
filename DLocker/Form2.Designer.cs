eaf41ec0d2fa62144a099d587e90c7059ee82f9f
l1ghth4t <pirogov20240@gmail.com>
2020-07-13 17:39:18 +0300

    Fixing vulnerability

--- DLocker/Form2.Designer.cs
+++ DLocker/Form2.Designer.cs
@@ -147,7 +147,7 @@
             // timer2
             // 
             this.timer2.Enabled = true;
-            this.timer2.Interval = 300;
+            this.timer2.Interval = 50;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form2
